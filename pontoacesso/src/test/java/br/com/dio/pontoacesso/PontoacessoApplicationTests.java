94cb5e9100e423c6bfd19dd978ca11cafc91a605
Danilo <Danilo@DESKTOP>
2021-08-09 09:30:10 -0300

    Primeiro Commit

---
+++ pontoacesso/src/test/java/br/com/dio/pontoacesso/PontoacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.pontoacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoacessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
